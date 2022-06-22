80bc42c3c0d92130addb02aef38bb83a77165c31
HoangKien <hoangkien@gmail.com>
2022-06-22 15:12:32 +0700

    Save code before test.

--- src/main/java/com/example/paymentservice/controller/WalletController.java
+++ src/main/java/com/example/paymentservice/controller/WalletController.java
@@ -1,9 +1,24 @@
 package com.example.paymentservice.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.paymentservice.entity.TransactionHistory;
+import com.example.paymentservice.entity.Wallet;
+import com.example.paymentservice.service.WalletService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+@CrossOrigin("*")
 @RestController
 @RequestMapping(path = "api/v1/wallet")
 public class WalletController {
+    @Autowired
+    WalletService walletService;
+    @RequestMapping(method = RequestMethod.GET)
+    public List<TransactionHistory> findAll(){
+        return walletService.findAll();
+    }
+    @RequestMapping(path = "{id}",method = RequestMethod.GET)
+    public Wallet getWallet(@PathVariable int id){
+        return walletService.getWallet(id);
+    }
 }
--- src/main/java/com/example/paymentservice/service/WalletService.java
+++ src/main/java/com/example/paymentservice/service/WalletService.java
@@ -1,7 +1,29 @@
 package com.example.paymentservice.service;
 
+import com.example.paymentservice.entity.TransactionHistory;
+import com.example.paymentservice.entity.Wallet;
+import com.example.paymentservice.repository.RepositoryTransactionHistory;
+import com.example.paymentservice.repository.RepositoryWallet;
+import org.apache.catalina.LifecycleState;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class WalletService {
+    @Autowired
+    RepositoryTransactionHistory repositoryTransactionHistory;
+    @Autowired
+    RepositoryWallet repositoryWallet;
+    public List<TransactionHistory> findAll(){
+        return repositoryTransactionHistory.findAll();
+    }
+    public Wallet getWallet(int id){
+        Wallet wallet = repositoryWallet.findById(id).orElse(null);
+        if (wallet != null){
+            return wallet;
+        }
+        return null;
+    }
 }
